b2dcf2fdb2d3fbb6ebaa99fd9d9ff38129c5392c
Andrea Amantini <lo.zampino@gmail.com>
2023-07-20 17:22:03 +0200

    Use enter to add

--- notebooks/todo.clj
+++ notebooks/todo.clj
@@ -13,12 +13,14 @@
   {:transform-fn clerk/mark-presented
    :render-fn '(fn [{:as m :task/keys [description completed? id]} _]
                  (println "task" (str id) completed?)
-                 [:div.mb-1.flex.bg-amber-200.border.border-amber-400.rounded-md
-                  [:div.mt-1.mx-2 [:input {:type :checkbox :checked (boolean completed?)
-                                           :on-change (fn [e]
-                                                        (.then (nextjournal.clerk.render/clerk-eval
-                                                                {:recompute? true}
-                                                                (list 'update-task (str id) 'assoc :task/completed? (.. e -target -checked)))))}]]
+                 [:div.mb-1.flex.bg-amber-200.border.border-amber-400.rounded-md.p-2
+                  [:div [:input.mt-2.ml-3 {:type :checkbox :checked (boolean completed?)
+                                 :class (str "appearance-none h-4 w-4 rounded bg-amber-300 border border-amber-400 relative"
+                                             "checked:border-amber-600 checked:bg-amber-600 checked:bg-no-repeat checked:bg-contain")
+                                 :on-change (fn [e]
+                                              (.then (nextjournal.clerk.render/clerk-eval
+                                                      {:recompute? true}
+                                                      (list 'update-task (str id) 'assoc :task/completed? (.. e -target -checked)))))}]]
                   [:div.text-xl.ml-2.mb-0.font-sans description]])})
 
 (def tasks-viewer
@@ -28,18 +30,25 @@
 {::clerk/visibility {:code :hide :result :show}}
 
 (clerk/with-viewer
- '(fn [_ _]
-    (let [text (nextjournal.clerk.render.hooks/use-state nil)]
+  '(fn [_ _]
+     (let [text (nextjournal.clerk.render.hooks/use-state nil)
+          ref (nextjournal.clerk.render.hooks/use-ref nil)
+          handle-key-press (nextjournal.clerk.render.hooks/use-callback
+                            (fn [e]
+                              (when (and (= "Enter" (.-key e)) (= (.-target e) @ref) (not-empty @text))
+                                (reset! text nil)
+                                (nextjournal.clerk.render/clerk-eval {:recompute? true} (list 'add-task @text)))) [text])]
+
+      (nextjournal.clerk.render.hooks/use-effect
+       (fn []
+         (.addEventListener js/window "keydown" handle-key-press)
+         #(.removeEventListener js/window "keydown" handle-key-press)) [handle-key-press])
+
       [:div.p-1.flex.bg-amber-100.border-amber-200.border.rounded-md.h-10.w-full.pl-8.font-sans.text-xl
-       [:button.text-xs.px-2.rounded-md.bg-amber-300.mr-2 {:on-click (fn [^js e]
-                                                                       (when (not-empty @text)
-                                                                         (reset! text nil)
-                                                                         (nextjournal.clerk.render/clerk-eval
-                                                                          {:recompute? true}
-                                                                          (list 'add-task @text))))} "Add"]
-       [:input.bg-amber-100.focus:outline-none {:on-change #(reset! text (.. % -target -value))
-                                                :placeholder "Enter a task…"
-                                                :value @text :type "text"}]])) nil)
+       [:input.bg-amber-100.focus:outline-none.text-md.w-full
+        {:on-change #(reset! text (.. % -target -value))
+         :placeholder "Enter text and press Enter…" :ref ref
+         :value @text :type "text"}]])) nil)
 
 (clerk/with-viewer tasks-viewer !tasks)
 
@@ -75,17 +84,19 @@
         {:keys [db-after]} (d/transact! conn [updated-entity])]
     (->map (d/entity db-after ref))))
 
+(defn remove-task [id]
+  (d/transact conn [[:db/retractEntity [:task/id (parse-uuid id)]]]))
+
 (reset! !tasks (tasks))
 
-#_
-(comment
-  d/get-conn
-  (fs/delete-tree "/tmp/garden/storage/todo")
-  (fs/list-dir "/tmp/garden/storage/todo")
-  (->> (d/q '[:find [?t ...] :where [?t :task/id]]
-            (d/db conn))
-       (map #(d/entity (d/db conn) %))
-       (sort-by :db/created-at >)
-       (map d/touch)
-
-       ))
+#_(comment
+    (def id *1)
+    d/get-conn
+    (fs/delete-tree "/tmp/garden/storage/todo")
+    (fs/list-dir "/tmp/garden/storage/todo")
+    (->> (d/q '[:find [?t ...] :where [?t :task/id]]
+              (d/db conn))
+         (map #(d/entity (d/db conn) %))
+         (sort-by :db/created-at >)
+         (map d/touch)
+         (first) :task/id str))
